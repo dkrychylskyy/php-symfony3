e52b5ba3e51d267fcfeab6d56ae9918b090a6897
dimitry.krychylskyy <d.krychylskyy@gmail.com>
2018-05-18 16:15:25 +0200

    Installation les dependes pour BD. Create classe User

---
+++ src/AppBundle/Entity/User.php
@@ -0,0 +1,17 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: dimitry.krychylskyy
+ * Date: 18/05/2018
+ * Time: 16:12
+ */
+
+namespace AppBundle\Entity;
+
+
+class User
+{
+    private $nom;
+    private $prenom;
+    private $email;
+}
